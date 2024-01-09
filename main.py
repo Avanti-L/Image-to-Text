492c8ad326ecb0dd9ee076e5ba6725a16f099630
Avanti-L <avantilabde8@gmail.com>
2024-01-09 19:47:53 +0530

    Add tesseract path in environment variables

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = \
-    r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 st.title("Image to Text Converter")
 
